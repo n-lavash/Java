bbbb8b5c7186028fa901f1fe7e8e8bdf6c4e0f31
n-lavash <levashkinaanastasia@gmail.com>
2022-03-11 02:02:15 +0400

    create class about dishes

---
+++ src/com/ssu/anastasia_levashkina/java/lesson2/task1/Dishes/Dishes.java
@@ -0,0 +1,10 @@
+package com.ssu.anastasia_levashkina.java.lesson2.task1.Dishes;
+
+public class Dishes {
+
+    private String title;
+
+    public Dishes(String title) {
+        this.title = title;
+    }
+}
