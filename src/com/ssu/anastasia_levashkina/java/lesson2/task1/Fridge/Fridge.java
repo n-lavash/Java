fb0603508041c84cf00ee48743ec4bda9a0ccce1
n-lavash <levashkinaanastasia@gmail.com>
2022-03-14 23:51:27 +0400

    add new methods

--- src/com/ssu/anastasia_levashkina/java/lesson2/task1/Fridge/Fridge.java
+++ src/com/ssu/anastasia_levashkina/java/lesson2/task1/Fridge/Fridge.java
@@ -4,10 +4,12 @@ import com.ssu.anastasia_levashkina.java.lesson2.task1.Dishes.Dishes;
 import com.ssu.anastasia_levashkina.java.lesson2.task1.Foods.Foods;
 
 public interface Fridge {
-    void addFood();
-    void addDish();
-    Foods getFood();
-    Dishes getDish();
-    Foods searchFood();
-    Foods sortFoods();
+
+    void addFoods(String type, int number);
+    void addDish(String title, String description);
+    Foods getFood(String type, String title);
+    Dishes getDish(String title);
+    Foods[] findFood();
+    void sort(String option);
+    void showAll(boolean dishes);
 }
