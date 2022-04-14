0e2d07993c89c2d414b4b541cca0deb1ddb589ff
n-lavash <levashkinaanastasia@gmail.com>
2022-04-15 02:33:05 +0400

    add 3 task

---
+++ src/lesson4/task1/DynamicArray.java
@@ -0,0 +1,105 @@
+package lesson4.task1;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DynamicArray<T> implements Iterable<T>{
+    T[] array;
+    int number;
+
+    public DynamicArray(Class<T> MyClass, int MyCapacity) {
+        array = (T[]) Array.newInstance(MyClass, MyCapacity);
+    }
+
+    public DynamicArray(Class<T> MyClass) {
+        int DefaultCapacity = 5;
+        array = (T[]) Array.newInstance(MyClass, DefaultCapacity);
+    }
+
+    public void createArray() {
+        int capacity = array.length * 2;
+        T[] copyArray = Arrays.copyOf(array, capacity);
+        try {
+            for(int i = 0 ; i < array.length; i++){
+
+                copyArray[i] = array[i];
+            }
+            array = copyArray;
+
+        }
+        catch (ArrayStoreException e){
+            e.printStackTrace();
+
+        }
+    }
+
+    public void add(T elem) {
+        if(number + 1 == array.length){
+           createArray();
+        }
+
+        array[number] = elem;
+        number++;
+    }
+
+    public T get(int index) {
+        return array[index];
+    }
+
+    public void set(T elem, int index) {
+        T tmpElem = array[index];
+        array[index] = elem;
+        for(int i = index + 1 ; i < array.length;i++ ){
+            T tmp = array[i];
+            array[i] = tmpElem;
+            tmpElem = tmp;
+        }
+        number++;
+    }
+
+    public void remove(int index) {
+        array[index] = array[index+1];
+        for(int i = index + 1 ; i < array.length - 1 ; i++) {
+            array[i] = array[i+1];
+        }
+        number--;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("");
+
+        for (int i = 0; i < number; i++) {
+
+            result.append(array[i]).append("[").append(i).append("] ");
+        }
+        return result + "\nnumber = " + number + "\n";
+    }
+
+
+    public class MyIterator implements Iterator<T> {
+
+        int current = 0;
+
+        @Override
+        public boolean hasNext() {
+            return number > current ;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return array[current++];
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyIterator();
+    }
+}
---
+++ src/lesson4/task1/Main.java
@@ -0,0 +1,36 @@
+package lesson4.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        DynamicArray<Integer> array = new DynamicArray<>(Integer.class, 3);
+        System.out.println("Проверка массива:");
+        System.out.println(array);
+        
+        System.out.println("Добавляем элементы в конец массива:");
+        array.add(9);
+        array.add(8);
+        array.add(7);
+        System.out.println(array);
+        array.add(6);
+        array.add(5);
+        System.out.println(array);
+
+        System.out.println("Добавляем элемент в середину массива");
+        array.set(100, 2);
+        System.out.println(array);
+
+        System.out.println("Удаление элемента:");
+        array.remove(3);
+        System.out.println(array);
+
+        System.out.println("Получение элемента по индексу");
+        System.out.println(array.get(4));
+
+        System.out.println("Проверка работы foreach");
+        for (Integer elem:
+             array) {
+            System.out.println(elem);
+        }
+        
+    }
+}
